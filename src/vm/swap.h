330e69fd3b06dabc88060ce87265fd36484792e1
bilal <bill.ahmed@mail.utoronto.ca>
2021-03-22 21:44:51 -0400

    forgot some design docs stuff

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -10,7 +10,7 @@ struct block* global_swap;
 // Prevent reading/writing at the same time!
 struct lock swap_modify_lock;
 
-// Maximum number of entires that can be swapped
+// Maximum number of entries that can be swapped
 #define MAX_SWAP_ENTRIES 1024
 
 // Number of blocks to write in order to fit a single page.
